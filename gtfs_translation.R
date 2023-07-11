4c18a2d0c9568763ae7ebe3a66e3594bb09b28cc
idshklein <idshklein@gmail.com>
2023-07-12 00:06:53 +0300

    gtfs2gps bug fixed

--- gtfs_translation.R
+++ gtfs_translation.R
@@ -1,10 +1,10 @@
-pacman::p_load(tidyverse,hms,gtfsrouter,gtfstools,data.table,mapview)
-agency <- tibble(agency_id = 1,
+pacman::p_load(tidyverse,hms,gtfsrouter,gtfstools,data.table,mapview,lubridate,gtfs2gps,gganimate)
+agency <- tibble(agency_id = "1",
                  agency_name = "1",
-                 agency_url = "1",
+                 agency_url = "http://notintersting.com",
                  agency_timezone = "Asia/Jerusalem") %>% 
   as.data.table()
-calendar <- tibble(service_id = 1,
+calendar <- tibble(service_id = "1",
                    monday=1,
                    tuesday=1,
                    wednesday=1,
@@ -12,8 +12,8 @@ calendar <- tibble(service_id = 1,
                    friday=1,
                    saturday=1,
                    sunday=1,
-                   start_date = "20230101",
-                   end_date = "20300101") %>% 
+                   start_date = ymd("2023-01-01"),
+                   end_date = ymd("2030-01-01")) %>% 
   as.data.table()
 routes <- tibble(route_id = lines_alg$name,
                  route_short_name = route_id,
@@ -29,7 +29,7 @@ shapes <- lines_alg %>% st_coordinates() %>%
   mutate(shape_id = as.character(shape_id)) %>% 
   as.data.table()
 trips <- tibble(route_id = lines_alg$name,
-                service_id = 1,
+                service_id = "1",
                 trip_id = route_id) %>% 
   rowid_to_column("shape_id") %>% 
   mutate(shape_id = as.character(shape_id)) %>% 
@@ -41,7 +41,7 @@ stop_times <- tibble(trip_id = lines_alg$name %>% rep(each = 2),
                      ) %>% 
   group_by(trip_id) %>% 
   mutate(stop_sequence = row_number(),
-         stop_id = map2_chr(trip_id,stop_sequence,~str_split(.x,"-",simplify = T)[[.y]])) %>% 
+         stop_id = map2_chr(trip_id,stop_sequence,~str_split(.x,"-",simplify = T)[[ifelse(1==.y,2,1)]])) %>% 
   as.data.table()
 
 frequencies <- tibble(trip_id = lines_alg$name,
@@ -49,10 +49,10 @@ frequencies <- tibble(trip_id = lines_alg$name,
                       end_time= "23:59:59",
                       headway_secs = as.integer(180)) %>% 
   as.data.table()
-stops <- tibble(stop_name = stops_alg$values,
+stops <- tibble(stop_id = stops_alg$values,
+                stop_name = stop_id,
                 stop_lat = stops_alg %>% st_coordinates() %>% `[`(,2),
                 stop_lon = stops_alg %>% st_coordinates() %>% `[`(,1))%>% 
-  rowid_to_column("stop_id") %>% 
   select(c("stop_id","stop_name","stop_lat","stop_lon")) %>% 
   mutate(stop_id = as.character(stop_id)) %>% 
   as.data.table()
@@ -67,5 +67,12 @@ gtfs$shapes <- shapes
 gtfs$stop_times <- stop_times
 gtfs$stops <- stops
 gtfs$trips <- trips
-frequencies_to_stop_times(gtfs)
-gtfs %>% gtfstools::get_trip_geometry(file = "shapes") %>% mapview()
+gtfs <- frequencies_to_stop_times(gtfs)
+write_gtfs(gtfs,"ashdod_gtfs.zip")
+# gtfs %>% gtfstools::get_trip_geometry(file = "shapes") %>% mapview()
+gtfs %>% gtfstools::get_trip_length(file = "shapes") %>% pull(length) %>% sum() %>% `*`(365) %>% `/`(230000)
+gtfs %>%gtfstools::get_trip_speed()
+# validate_gtfs(gtfs,"C:/Users/idshk/Downloads/valid","C:/Users/idshk/Downloads/valid/gtfs-validator-v4.0.0.jar")
+timer <- gtfs2gps(gtfs)
+timer%>% filter(timestamp<as.ITime("01:00:00")) %>% st_as_sf(coords = c("shape_pt_lon","shape_pt_lat"),crs = 4326) %>% st_write("anim.gpkg",delete_layer =T)
+
